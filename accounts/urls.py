17f45b20bb621e08e2467fcdcf361c51e4126954
amirhossein <79211332+capamir@users.noreply.github.com>
2023-06-15 17:37:49 +0330

    reset password

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,23 @@
 from django.urls import path, include
 from . import views
+from django.contrib.auth import views as auth_views
+
 
 app_name = 'accounts'
+
+password = [
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="accounts/password/reset_password.html"),
+         name="reset_password"),
+
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="accounts/password/reset_password_sent.html"),
+         name="password_reset_done"),
+
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="accounts/password/reset.html"),
+         name="password_reset_confirm"),
+
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="accounts/password/reset_password_complete.html"),
+         name="password_reset_complete"),
+]
 
 skill = [
     path('create/', views.CreateSkillView.as_view(), name='create-skill'),
@@ -30,5 +46,6 @@ urlpatterns = [
 
     path('skill/', include(skill)),
     path('message/', include(message)),
+    path('password/', include(password)),
 
 ]
